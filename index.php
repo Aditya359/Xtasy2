e5801ab48ea88e5d53e5577a5f62e296f914779b
aditya359 <laluprasad.lenka95@gmail.com>
2016-04-11 23:21:04 -0700

    stylesheets added

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+		<?php
+
+    <!-- GOOGLE FONTS -->
+
+						<link href='assets/css/googlefonts.css' rel='stylesheet' type='text/css'>
+						<link href="assets/css/bootstrap.css" rel="stylesheet">
+						<link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+     				<script type="text/javascript" src="assets/js/jquery-1.11.0.min.js"></script>
 	</head>
 
 
